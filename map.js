3be5d50e263b74b792360d99166958b3ef9d3b51
gatnalysing <137360061+gatnalysing@users.noreply.github.com>
2023-08-02 07:50:12 +0000

    hitastig bætt við

--- map.js
+++ map.js
@@ -7,48 +7,28 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 18,
 }).addTo(map);
 
-// Load the CSV file and generate circle markers
-fetch('light_posts.csv')
-  .then(response => response.text())
+// Fetch the data from the Adafruit IO API
+fetch('https://io.adafruit.com/api/v2/davidjrb/feeds/hitastig/data?limit=1')
+  .then(response => response.json())
   .then(data => {
-    var lightPosts = parseCSV(data);
-    lightPosts.forEach(function(post) {
-      var circle = L.circle([post.latitude, post.longitude], {
-        color: post.colour, // Set the color based on the 'colour' column in the CSV
-        fillColor: 'black', // Set the fill color to black
-        fillOpacity: 0.8,
-        radius: getMarkerRadius(map.getZoom()) // Calculate the radius based on the current zoom level
-      }).addTo(map);
+    // Assuming the latest value is in the first array element
+    var latestData = data[0];
+    var temperature = parseFloat(latestData.value);
+    var circle = L.circle([64.129327, -21.8183472], {
+      color: 'black',
+      fillColor: 'black',
+      fillOpacity: 0.8,
+      radius: getMarkerRadius(map.getZoom())
+    }).addTo(map);
 
-      circle.bindPopup(post.name);
-    });
+    circle.bindPopup('hitastig<br>Temperature: ' + temperature.toFixed(2) + '°C');
   });
-
-// Parse CSV data into an array of objects
-function parseCSV(csv) {
-  var lines = csv.split('\n');
-  var result = [];
-  var headers = lines[0].split(',');
-
-  for (var i = 1; i < lines.length; i++) {
-    var obj = {};
-    var currentLine = lines[i].split(',');
-
-    for (var j = 0; j < headers.length; j++) {
-      obj[headers[j].trim()] = currentLine[j].trim();
-    }
-
-    result.push(obj);
-  }
-
-  return result;
-}
 
 // Calculate the marker radius based on the current zoom level
 function getMarkerRadius(zoom) {
-  var baseRadius = 10; // Adjust this value to control the initial dot size
-  var maxRadius = 100; // Adjust this value to set the maximum dot size
-  var zoomThreshold = 16; // Adjust this value to set the zoom level where the dot size stops increasing
+  var baseRadius = 10;
+  var maxRadius = 100;
+  var zoomThreshold = 16;
 
   var adjustedRadius = baseRadius * Math.pow(2, zoomThreshold - zoom);
   return Math.min(adjustedRadius, maxRadius);
