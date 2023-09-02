495e797da38ca8347c5460fa04bfc11ea8baeca2
gatnalysing <137360061+gatnalysing@users.noreply.github.com>
2023-09-02 16:50:53 +0000

    back to csv

--- map.js
+++ map.js
@@ -7,38 +7,38 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 18,
 }).addTo(map);
 
-// Fetch the data from the Adafruit IO API
-fetch('https://io.adafruit.com/api/v2/davidjrb/feeds/hitastig/data?limit=1')
-  .then(response => response.json())
-  .then(data => {
-    // Assuming the latest value is in the first array element
-    var latestData = data[0];
-    var temperature = parseFloat(latestData.value);
-    var circle = L.circle([64.129327, -21.8183472], {
-      color: 'black',
-      fillColor: 'black',
-      fillOpacity: 0.8,
-      radius: getMarkerRadius(map.getZoom())
-    }).addTo(map);
-
-    circle.bindPopup('hitastig<br>Temperature: ' + temperature.toFixed(2) + '°C');
+// Function to create a custom marker
+function createCustomMarker() {
+  return L.divIcon({
+    className: 'custom-marker',
+    iconSize: [15, 15]
   });
-
-// Calculate the marker radius based on the current zoom level
-function getMarkerRadius(zoom) {
-  var baseRadius = 10;
-  var maxRadius = 100;
-  var zoomThreshold = 16;
-
-  var adjustedRadius = baseRadius * Math.pow(2, zoomThreshold - zoom);
-  return Math.min(adjustedRadius, maxRadius);
 }
+
+// Fetch CSV data and create markers
+fetch('light_posts.csv')
+  .then(response => response.text())
+  .then(csvData => {
+    var parsedData = Papa.parse(csvData, { header: true }).data;
+    
+    parsedData.forEach(row => {
+      var lat = parseFloat(row.latitude);
+      var lng = parseFloat(row.longitude);
+      var marker = L.marker([lat, lng], { icon: createCustomMarker() }).addTo(map);
+
+      // Customize the popup content based on your CSV columns
+      var popupContent = '<strong>' + row.title + '</strong><br>' +
+                         'Description: ' + row.description;
+      marker.bindPopup(popupContent);
+    });
+  })
+  .catch(error => console.error('Error fetching CSV data:', error));
 
 // Update marker sizes when the zoom level changes
 map.on('zoomend', function() {
   map.eachLayer(function(layer) {
-    if (layer instanceof L.Circle) {
-      layer.setRadius(getMarkerRadius(map.getZoom()));
+    if (layer instanceof L.Marker) {
+      layer.setIcon(createCustomMarker());
     }
   });
 });
