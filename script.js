bb273e1146e0c49c64b613541321043ebdda7084
gatnalysing <137360061+gatnalysing@users.noreply.github.com>
2023-09-02 21:31:01 +0000

    30k and zoom display

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
+// Custom control to display zoom level
+L.Control.ZoomLabel = L.Control.extend({
+    options: {
+        position: 'topleft'
+    },
+    onAdd: function(map) {
+        this._container = L.DomUtil.create('div', 'leaflet-control-zoomlabel');
+        this.update();
+        return this._container;
+    },
+    update: function() {
+        if (this._map) {
+            this._container.innerHTML = 'Zoom: ' + this._map.getZoom();
+        }
+    }
+});
+
+// Instantiate the map
 let map = L.map('map').setView([64.133, -21.898], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
+    attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+// Add the custom zoom level control
+let zoomLabelControl = new L.Control.ZoomLabel().addTo(map);
+
+// Update the zoom label when the zoom level changes
+map.on('zoomend', function() {
+    zoomLabelControl.update();
+});
+
+// Fetching the CSV data and adding points to the map (assuming CSV is properly formatted)
 fetch('data.csv')
-  .then(response => response.text())
-  .then(csvString => {
-    let data = Papa.parse(csvString, { header: true, skipEmptyLines: true }).data;
-
-    for (let item of data) {
-      let circle = L.circleMarker([item.latitude, item.longitude], {
-        color: item.colour,
-        fillColor: item.colour,
-        fillOpacity: 0.5,
-        radius: 5
-      }).addTo(map);
-      circle.bindPopup(`<b>Name:</b> ${item.name}<br><b>Type:</b> ${item.type}<br><b>Status:</b> ${item.status}`);
-    }
-
-    // Force the map to adjust its size
-    map.invalidateSize();
-  });
+    .then(response => response.text())
+    .then(data => {
+        const lines = data.split('\n').slice(1);  // Split by lines and ignore the header
+        lines.forEach(line => {
+            const [lat, lon, name, type, status, colour] = line.split(',');
+            if (lat && lon) {
+                L.circleMarker([parseFloat(lat), parseFloat(lon)], {
+                    color: colour.trim(),
+                    radius: 5
+                }).bindPopup(`<b>${name.trim()}</b><br>Type: ${type.trim()}<br>Status: ${status.trim()}`).addTo(map);
+            }
+        });
+    });
