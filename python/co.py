1ca079078d368a3074e1dd40467316a79d7ca762
gatnalysing <137360061+gatnalysing@users.noreply.github.com>
2023-09-07 16:12:25 +0000

    overcoming commitment issues from replit's nixos with github

--- python/co.py
+++ python/co.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import os
 from datetime import datetime
 from pyproj import Transformer
 
@@ -19,7 +20,15 @@ def format_coordinate(coord_str):
     return float(coord_str[:-3] + '.' + coord_str[-3:])
 
 def main(input_file):
-    output_file = input_file + '-umbreytt' + datetime.now().strftime('%Y%m%d%H%M')
+  
+    # Extract the file name without the extension and its extension
+    base_name, extension = os.path.splitext(input_file)
+    # Construct the new output file name
+    output_file = f"{base_name}-umbreytt{datetime.now().strftime('%Y%m%d%H%M')}{extension}"
+    
+    # Step 1: Determine the number of lines in the input file
+    with open(input_file, 'r') as f:
+        total_lines = sum(1 for line in f) - 1  # Subtract 1 for the header
 
     with open(input_file, 'r') as csv_input, open(output_file, 'w', newline='') as csv_output:
         reader = csv.DictReader(csv_input)
@@ -29,7 +38,8 @@ def main(input_file):
         # Write header to the output file
         writer.writeheader()
 
-        for row in reader:
+        # Process each row and print progress
+        for index, row in enumerate(reader, 1):
             x = format_coordinate(row['X'])
             y = format_coordinate(row['Y'])
             lat, lon = convert_coordinates(x, y)
@@ -43,11 +53,14 @@ def main(input_file):
                 "colour": "blue" if row['HLUTVERK'] == "Götuljós" else "green",
                 "DNR": row['DNR'],
                 "EIGANDI": row['EIGANDI'],
-                "DAGS": row['DAGSINNSETNINGAR'].split()[0]  # Extract the date without time
+                "DAGS": row['DAGSINNSETNINGAR'].split()[0] if row['DAGSINNSETNINGAR'] else "1776/07/04"  # Extract the date without time
             }
             writer.writerow(new_row)
 
-    print("Conversion completed successfully.")
+            # Step 2: Print progress
+            print(f"Progress: Converting line {index} of {total_lines}", end='\r')  # end='\r' will overwrite the line in the console, keeping it neat
+
+    print("\nConversion completed successfully.")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
