6af33f6d8aa35dae2e21d5e624aee352591e2342
gatnalysing <137360061+gatnalysing@users.noreply.github.com>
2024-08-22 10:47:24 +0000

    Update mapInitialization.js

--- JS/mapInitialization.js
+++ JS/mapInitialization.js
@@ -1,5 +1,5 @@
 // Instantiate the map
-let map = L.map('map').setView([64.133, -21.898], 13);
+let map = L.map('map').setView([64.14664697028653, -21.933761620456423], 16);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
